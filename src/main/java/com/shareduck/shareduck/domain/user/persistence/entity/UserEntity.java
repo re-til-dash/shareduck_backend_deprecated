d329d0721bb5eeaf51b977508de936cea32881fb
SigLee <siglee2247@gmail.com>
2024-07-28 12:23:00 +0900

    feat: add auditing class

--- src/main/java/com/shareduck/shareduck/domain/user/persistence/entity/UserEntity.java
+++ src/main/java/com/shareduck/shareduck/domain/user/persistence/entity/UserEntity.java
@@ -1,7 +1,14 @@
 package com.shareduck.shareduck.domain.user.persistence.entity;
 
+import com.shareduck.shareduck.common.audit.BaseEntity;
 import com.shareduck.shareduck.domain.user.persistence.enums.UserRole;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,15 +16,30 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "User")
+@Table(name = "user")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserEntity {
+public class UserEntity extends BaseEntity {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 100)
     private Long id;
     private String email;
     private String password;
     private UserRole role;
+    private String profile;
+    private String nickname;
+    private String phone;
+    private LocalDateTime lastConnect;
+
+    public UserEntity addPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public void addNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
