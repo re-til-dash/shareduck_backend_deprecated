3f34d15402a49ca9b20e062c6239dec3e3ba1ff6
SigLee <siglee2247@gmail.com>
2024-09-21 16:12:41 +0900

    feat: add swagger setting

--- src/main/java/com/shareduck/shareduck/common/config/SwaggerConfig.java
+++ src/main/java/com/shareduck/shareduck/common/config/SwaggerConfig.java
@@ -2,42 +2,93 @@ package com.shareduck.shareduck.common.config;
 
 import static org.springframework.security.config.Elements.JWT;
 
+import com.shareduck.shareduck.common.security.filter.JwtAuthenticationFilter;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+    private final ApplicationContext applicationContext;
 
-    @Bean
-    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
 
-        Info info = new Info()
+    private Info apiInfo(String appVersion) {
+        return new Info()
             .title("ShareDuck Server Swagger")
             .version(appVersion)
             .description("[USER] : 사용자 권한 User <br> [ALL] 비로그인 사용자");
-
-        String securitySchemeName = HttpHeaders.AUTHORIZATION;
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
-        Components components = new Components()
-            .addSecuritySchemes(securitySchemeName, new SecurityScheme()
-                .name(securitySchemeName)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat(JWT));
-
-        return new OpenAPI()
-            .info(info)
-            .addSecurityItem(securityRequirement)
-            .components(components);
     }
+
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
+        SecurityScheme bearAuth = new SecurityScheme().name("Bearer Authentication").type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("Bearer");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearAuth");
+        return new OpenAPI()
+            .components(new Components().addSecuritySchemes("bearAuth", bearAuth))
+            .addSecurityItem(securityRequirement)
+            .info(apiInfo(appVersion));
+    }
+
+    @Bean
+    public OpenApiCustomizer authLoginOpenApi() {
+        FilterChainProxy filterChainProxy = applicationContext.getBean(
+            AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME, FilterChainProxy.class);
+        return openAPI -> {
+            for (SecurityFilterChain filterChain : filterChainProxy.getFilterChains()) {
+                Optional<JwtAuthenticationFilter> optionalFilter =
+                    filterChain.getFilters().stream()
+                        .filter(JwtAuthenticationFilter.class::isInstance)
+                        .map(JwtAuthenticationFilter.class::cast)
+                        .findAny();
+                if (optionalFilter.isPresent()) {
+                    Operation operation = new Operation();
+                    Schema<?> schema = new ObjectSchema()
+                        .addProperty("email", new StringSchema())
+                        .addProperty("password",new StringSchema());
+                    RequestBody requestBody = new RequestBody().content(
+                        new Content().addMediaType(org.springframework.http.MediaType.APPLICATION_JSON_VALUE,
+                            new MediaType().schema(schema)));
+                    operation.requestBody(requestBody);
+                    ApiResponses apiResponses = new ApiResponses();
+                    apiResponses.addApiResponse(String.valueOf(HttpStatus.OK.value()),
+                        new ApiResponse().description(HttpStatus.OK.getReasonPhrase()));
+                    apiResponses.addApiResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                        new ApiResponse().description(HttpStatus.BAD_REQUEST.getReasonPhrase()));
+                    operation.responses(apiResponses);
+                    operation.addTagsItem("AUTH");
+                    PathItem pathItem = new PathItem().post(operation);
+                    openAPI.getPaths().addPathItem("/api/login", pathItem);
+                }
+            }
+        };
+    }
+
 
     @Bean
     public GroupedOpenApi apiGroup() {
@@ -46,7 +97,7 @@ public class SwaggerConfig {
             "/api/**"
         };
         String[] pathToExclude = {};
-        return GroupedOpenApi.builder()
+        return commonApi()
             .group("All")
             .pathsToMatch(pathToMatch)
             .pathsToExclude(pathToExclude)
@@ -59,7 +110,7 @@ public class SwaggerConfig {
             "/api/v1/users/**"
         };
         String[] pathToExclude = {};
-        return GroupedOpenApi.builder()
+        return commonApi()
             .group("_USER")
             .pathsToMatch(pathToMatch)
             .pathsToExclude(pathToExclude)
@@ -72,7 +123,7 @@ public class SwaggerConfig {
             "/api/categories/**"
         };
         String[] pathToExclude = {};
-        return GroupedOpenApi.builder()
+        return commonApi()
             .group("_CATEGORY")
             .pathsToMatch(pathToMatch)
             .pathsToExclude(pathToExclude)
@@ -85,7 +136,7 @@ public class SwaggerConfig {
             "/api/memos/**"
         };
         String[] pathToExclude = {};
-        return GroupedOpenApi.builder()
+        return commonApi()
             .group("_MEMO")
             .pathsToMatch(pathToMatch)
             .pathsToExclude(pathToExclude)
@@ -98,7 +149,7 @@ public class SwaggerConfig {
             "/api/posts/**"
         };
         String[] pathToExclude = {};
-        return GroupedOpenApi.builder()
+        return commonApi()
             .group("_POST")
             .pathsToMatch(pathToMatch)
             .pathsToExclude(pathToExclude)
@@ -110,11 +161,14 @@ public class SwaggerConfig {
             "/api/upload/**"
         };
         String[] pathToExclude = {};
-        return GroupedOpenApi.builder()
+        return commonApi()
             .group("_UPLOAD")
             .pathsToMatch(pathToMatch)
             .pathsToExclude(pathToExclude)
             .build();
     }
-
+    private GroupedOpenApi.Builder commonApi(){
+        return GroupedOpenApi.builder()
+            .addOpenApiCustomizer(authLoginOpenApi());
+    }
 }
