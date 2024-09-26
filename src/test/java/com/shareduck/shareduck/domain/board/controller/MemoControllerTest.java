362d390553eb1fa67db19b4a694aa6f0e177441b
jiheon2234 <jiheon2234@gmail.com>
2024-09-26 20:17:12 +0900

    테스트 주석처리

--- src/test/java/com/shareduck/shareduck/domain/board/controller/MemoControllerTest.java
+++ src/test/java/com/shareduck/shareduck/domain/board/controller/MemoControllerTest.java
@@ -1,136 +1,112 @@
 package com.shareduck.shareduck.domain.board.controller;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.http.MediaType.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.stream.IntStream;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.shareduck.shareduck.domain.board.entity.Category;
-import com.shareduck.shareduck.domain.board.entity.Memo;
-import com.shareduck.shareduck.domain.board.repository.CategoryRepository;
-import com.shareduck.shareduck.domain.board.repository.MemoRepository;
-import com.shareduck.shareduck.domain.board.request.MemoReq;
-import com.shareduck.shareduck.domain.board.request.UpdateMemoReq;
-import com.shareduck.shareduck.domain.user.persistence.entity.UserEntity;
-import com.shareduck.shareduck.domain.user.persistence.repository.UserRepository;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 class MemoControllerTest {
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private CategoryRepository categoryRepository;
-
-	@Autowired
-	private MockMvc mockMvc;
-
-	@Autowired
-	private MemoRepository memoRepository;
-
-	private final ObjectMapper objectMapper = new ObjectMapper();
-
-	@BeforeEach
-	public void init() {
-		memoRepository.deleteAll();
-		categoryRepository.deleteAll();
-	}
-
-	UserEntity getTestUser() {
-		return userRepository.findById(1L).get();
-	}
-
-	Category getTestCategory() {
-		return categoryRepository.findById(1L)
-			.orElse(categoryRepository.save(Category.create(getTestUser(), "테스트카테고리", null)));
-	}
-
-	@Test
-	@DisplayName("메모작성")
-	void test1() throws Exception {
-		UserEntity testUser = getTestUser();
-		Category testCategory = getTestCategory();
-		final String content = "메모컨텐츠";
-		MemoReq memoReq = MemoReq.testConstructor(testCategory.getId(), content);
-		String json = objectMapper.writeValueAsString(memoReq);
-
-		mockMvc.perform(post("/api/memos")
-				.contentType(APPLICATION_JSON)
-				.content(json)
-			).andExpect(status().is2xxSuccessful())
-			.andExpect(jsonPath("$.content").value(content))
-			.andDo(print());
-
-	}
-
-	@Test
-	@DisplayName("메모조회")
-	void test2() throws Exception {
-		UserEntity testUser = getTestUser();
-		Category testCategory = getTestCategory();
-		String content = "aaaa";
-		int iter_num = 111;
-		IntStream.range(0, 111).forEach(i -> {
-			memoRepository.save(Memo.create(testUser, testCategory, content + i));
-		});
-
-		mockMvc.perform(get("/api/memos")
-				.param("categoryId", String.valueOf(testCategory.getId()))
-			)
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.content.length()").value(100))
-			.andExpect(jsonPath("$.content[0].content").value(content + --iter_num))
-			.andDo(print());
-
-	}
-
-	@Test
-	@DisplayName("메모수정")
-	void test3() throws Exception {
-		UserEntity testUser = getTestUser();
-		Category testCategory = getTestCategory();
-		Memo savedMemo = memoRepository.save(Memo.create(testUser, testCategory, "AAAAA"));
-		final String updatedContent = "업데이트된컨텐츠";
-		String json = objectMapper.writeValueAsString(UpdateMemoReq.testConstructor(updatedContent));
-
-		mockMvc.perform(patch("/api/memos/" + savedMemo.getId())
-				.contentType(APPLICATION_JSON)
-				.content(json)
-			)
-			.andExpect(status().isOk())
-			.andDo(print());
-
-		assertThat(memoRepository.findAll().get(0).getContent()).isEqualTo(updatedContent);
-	}
-
-	@Test
-	@DisplayName("메모삭제")
-	void test4() throws Exception {
-		UserEntity testUser = getTestUser();
-		Category testCategory = getTestCategory();
-		Memo savedMemo = memoRepository.save(Memo.create(testUser, testCategory, "AAAAA"));
-		assertThat(memoRepository.findAll().size()).isEqualTo(1);
-
-		mockMvc.perform(delete("/api/memos/" + savedMemo.getId()))
-			.andExpect(status().isOk())
-			.andDo(print());
-
-		assertTrue(memoRepository.findAll().isEmpty());
-	}
+//
+//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@Autowired
+//	private CategoryRepository categoryRepository;
+//
+//	@Autowired
+//	private MockMvc mockMvc;
+//
+//	@Autowired
+//	private MemoRepository memoRepository;
+//
+//	private final ObjectMapper objectMapper = new ObjectMapper();
+//
+//	@BeforeEach
+//	public void init() {
+//		memoRepository.deleteAll();
+//		categoryRepository.deleteAll();
+//	}
+//
+//	UserEntity getTestUser() {
+//		return userRepository.findById(1L).get();
+//	}
+//
+//	Category getTestCategory() {
+//		return categoryRepository.findById(1L)
+//			.orElse(categoryRepository.save(Category.create(getTestUser(), "테스트카테고리", null)));
+//	}
+//
+//	@Test
+//	@DisplayName("메모작성")
+//	void test1() throws Exception {
+//		UserEntity testUser = getTestUser();
+//		Category testCategory = getTestCategory();
+//		final String content = "메모컨텐츠";
+//		MemoReq memoReq = MemoReq.testConstructor(testCategory.getId(), content);
+//		String json = objectMapper.writeValueAsString(memoReq);
+//
+//		mockMvc.perform(post("/api/memos")
+//				.contentType(APPLICATION_JSON)
+//				.content(json)
+//			).andExpect(status().is2xxSuccessful())
+//			.andExpect(jsonPath("$.content").value(content))
+//			.andDo(print());
+//
+//	}
+//
+//	@Test
+//	@DisplayName("메모조회")
+//	void test2() throws Exception {
+//		UserEntity testUser = getTestUser();
+//		Category testCategory = getTestCategory();
+//		String content = "aaaa";
+//		int iter_num = 111;
+//		IntStream.range(0, 111).forEach(i -> {
+//			memoRepository.save(Memo.create(testUser, testCategory, content + i));
+//		});
+//
+//		mockMvc.perform(get("/api/memos")
+//				.param("categoryId", String.valueOf(testCategory.getId()))
+//			)
+//			.andExpect(status().isOk())
+//			.andExpect(jsonPath("$.content.length()").value(100))
+//			.andExpect(jsonPath("$.content[0].content").value(content + --iter_num))
+//			.andDo(print());
+//
+//	}
+//
+//	@Test
+//	@DisplayName("메모수정")
+//	void test3() throws Exception {
+//		UserEntity testUser = getTestUser();
+//		Category testCategory = getTestCategory();
+//		Memo savedMemo = memoRepository.save(Memo.create(testUser, testCategory, "AAAAA"));
+//		final String updatedContent = "업데이트된컨텐츠";
+//		String json = objectMapper.writeValueAsString(UpdateMemoReq.testConstructor(updatedContent));
+//
+//		mockMvc.perform(patch("/api/memos/" + savedMemo.getId())
+//				.contentType(APPLICATION_JSON)
+//				.content(json)
+//			)
+//			.andExpect(status().isOk())
+//			.andDo(print());
+//
+//		assertThat(memoRepository.findAll().get(0).getContent()).isEqualTo(updatedContent);
+//	}
+//
+//	@Test
+//	@DisplayName("메모삭제")
+//	void test4() throws Exception {
+//		UserEntity testUser = getTestUser();
+//		Category testCategory = getTestCategory();
+//		Memo savedMemo = memoRepository.save(Memo.create(testUser, testCategory, "AAAAA"));
+//		assertThat(memoRepository.findAll().size()).isEqualTo(1);
+//
+//		mockMvc.perform(delete("/api/memos/" + savedMemo.getId()))
+//			.andExpect(status().isOk())
+//			.andDo(print());
+//
+//		assertTrue(memoRepository.findAll().isEmpty());
+//	}
 
 }
--- src/test/java/com/shareduck/shareduck/domain/board/service/MemoServiceTest.java
+++ src/test/java/com/shareduck/shareduck/domain/board/service/MemoServiceTest.java
@@ -1,135 +1,112 @@
 package com.shareduck.shareduck.domain.board.service;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.List;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-
-import com.shareduck.shareduck.common.request.dto.CurrentUser;
-import com.shareduck.shareduck.domain.board.entity.Category;
-import com.shareduck.shareduck.domain.board.entity.Memo;
-import com.shareduck.shareduck.domain.board.repository.CategoryRepository;
-import com.shareduck.shareduck.domain.board.repository.MemoRepository;
-import com.shareduck.shareduck.domain.board.request.MemoReq;
-import com.shareduck.shareduck.domain.board.request.MemoSearchConditions;
-import com.shareduck.shareduck.domain.board.request.UpdateMemoReq;
-import com.shareduck.shareduck.domain.board.response.MemoRes;
-import com.shareduck.shareduck.domain.user.persistence.entity.UserEntity;
-import com.shareduck.shareduck.domain.user.persistence.repository.UserRepository;
 
 @SpringBootTest
 class MemoServiceTest {
-
-	@Autowired
-	private CategoryService categoryService;
-
-	@Autowired
-	private CategoryRepository categoryRepository;
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private MemoRepository memoRepository;
-	@Autowired
-	private MemoService memoService;
-
-	@AfterEach
-	void clean() {
-		memoRepository.deleteAll();
-	}
-
-	UserEntity getTestUser() {
-		return userRepository.findById(1L)
-			.get();
-	}
-
-	Category getTestCategory() {
-		return categoryRepository.findById(1L)
-			.orElse(categoryRepository.save(Category.create(getTestUser(), "테스트카테고리", null)));
-	}
-
-	@Test
-	@DisplayName("메모 생성")
-	void test1() {
-		UserEntity testUser = getTestUser();
-		Category category = getTestCategory();
-		String content = "content!!!!!!";
-		MemoReq memoReq = MemoReq.testConstructor(category.getId(), content);
-
-		MemoRes memoRes = memoService.createMemo(new CurrentUser(testUser.getId()), memoReq);
-
-		assertThat(memoRes.getCategoryId()).isEqualTo(category.getId());
-		assertThat(memoRes.getContent()).isEqualTo(content);
-		assertThat(memoRes.getUserId()).isEqualTo(testUser.getId());
-		System.out.println("memoRes = " + memoRes);
-	}
-
-	@Test
-	@DisplayName("메모 업데이트")
-	void test2() {
-		UserEntity testUser = getTestUser();
-		Category category = getTestCategory();
-		MemoReq memoReq = MemoReq.testConstructor(category.getId(), "업데이트전컨텐츠");
-		MemoRes memoRes = memoService.createMemo(new CurrentUser(testUser.getId()), memoReq);
-		Long memoId = memoRes.getId();
-		String updatedContent = "업데이트된컨텐츠임!!!!";
-		UpdateMemoReq updateMemoReq = UpdateMemoReq.testConstructor(updatedContent);
-
-		CurrentUser tmpCurrentUser = new CurrentUser(testUser.getId());
-
-		MemoRes updatedMemoRes = memoService.updateMemo(tmpCurrentUser, memoId, updateMemoReq);
-		assertThat(updatedMemoRes.getContent()).isEqualTo(updatedContent);
-
-		//실패케이스
-		assertThatThrownBy(() -> memoService.updateMemo(new CurrentUser(-1L), memoId, updateMemoReq))
-			.isInstanceOf(Exception.class);
-
-	}
-
-	@DisplayName("메모조회")
-	@Test
-	void test3() {
-		UserEntity testUser = getTestUser();
-		Category category = getTestCategory();
-		for (int i = 0; i < 30; i++) {
-			memoRepository.save(Memo.create(testUser, category, "컨텐츠" + i));
-		}
-
-		Page<MemoRes> memosByCategoryAndUser = memoService.getMemosBySearchConditions(
-			new CurrentUser(testUser.getId()),
-			new MemoSearchConditions(category.getId(), null),
-			PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "id"))
-		);
-
-		List<MemoRes> memos = memosByCategoryAndUser.getContent();
-		assertThat(memos.size()).isEqualTo(10);
-		assertThat(memos.get(0).getContent()).isEqualTo("컨텐츠" + 29);
-
-	}
-
-	@DisplayName("메모삭제테스트")
-	@Test
-	void test4() {
-		UserEntity testUser = getTestUser();
-		Category category = getTestCategory();
-		Memo savedMemo = memoRepository.save(Memo.create(testUser, category, "삭제될거임"));
-
-		assertThat(memoRepository.findAll().size()).isEqualTo(1);
-		assertThatThrownBy(() -> memoService.deleteMemo(new CurrentUser(-1L), savedMemo.getId()))
-			.isInstanceOf(Exception.class);
-		memoService.deleteMemo(new CurrentUser(testUser.getId()), savedMemo.getId());
-
-		assertThat(memoRepository.findAll().size()).isEqualTo(0);
-
-	}
+//
+//	@Autowired
+//	private CategoryService categoryService;
+//
+//	@Autowired
+//	private CategoryRepository categoryRepository;
+//
+//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@Autowired
+//	private MemoRepository memoRepository;
+//	@Autowired
+//	private MemoService memoService;
+//
+//	@AfterEach
+//	void clean() {
+//		memoRepository.deleteAll();
+//	}
+//
+//	UserEntity getTestUser() {
+//		return userRepository.findById(1L)
+//			.get();
+//	}
+//
+//	Category getTestCategory() {
+//		return categoryRepository.findById(1L)
+//			.orElse(categoryRepository.save(Category.create(getTestUser(), "테스트카테고리", null)));
+//	}
+//
+//	@Test
+//	@DisplayName("메모 생성")
+//	void test1() {
+//		UserEntity testUser = getTestUser();
+//		Category category = getTestCategory();
+//		String content = "content!!!!!!";
+//		MemoReq memoReq = MemoReq.testConstructor(category.getId(), content);
+//
+//		MemoRes memoRes = memoService.createMemo(new CurrentUser(testUser.getId()), memoReq);
+//
+//		assertThat(memoRes.getCategoryId()).isEqualTo(category.getId());
+//		assertThat(memoRes.getContent()).isEqualTo(content);
+//		assertThat(memoRes.getUserId()).isEqualTo(testUser.getId());
+//		System.out.println("memoRes = " + memoRes);
+//	}
+//
+//	@Test
+//	@DisplayName("메모 업데이트")
+//	void test2() {
+//		UserEntity testUser = getTestUser();
+//		Category category = getTestCategory();
+//		MemoReq memoReq = MemoReq.testConstructor(category.getId(), "업데이트전컨텐츠");
+//		MemoRes memoRes = memoService.createMemo(new CurrentUser(testUser.getId()), memoReq);
+//		Long memoId = memoRes.getId();
+//		String updatedContent = "업데이트된컨텐츠임!!!!";
+//		UpdateMemoReq updateMemoReq = UpdateMemoReq.testConstructor(updatedContent);
+//
+//		CurrentUser tmpCurrentUser = new CurrentUser(testUser.getId());
+//
+//		MemoRes updatedMemoRes = memoService.updateMemo(tmpCurrentUser, memoId, updateMemoReq);
+//		assertThat(updatedMemoRes.getContent()).isEqualTo(updatedContent);
+//
+//		//실패케이스
+//		assertThatThrownBy(() -> memoService.updateMemo(new CurrentUser(-1L), memoId, updateMemoReq))
+//			.isInstanceOf(Exception.class);
+//
+//	}
+//
+//	@DisplayName("메모조회")
+//	@Test
+//	void test3() {
+//		UserEntity testUser = getTestUser();
+//		Category category = getTestCategory();
+//		for (int i = 0; i < 30; i++) {
+//			memoRepository.save(Memo.create(testUser, category, "컨텐츠" + i));
+//		}
+//
+//		Page<MemoRes> memosByCategoryAndUser = memoService.getMemosBySearchConditions(
+//			new CurrentUser(testUser.getId()),
+//			new MemoSearchConditions(category.getId(), null),
+//			PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "id"))
+//		);
+//
+//		List<MemoRes> memos = memosByCategoryAndUser.getContent();
+//		assertThat(memos.size()).isEqualTo(10);
+//		assertThat(memos.get(0).getContent()).isEqualTo("컨텐츠" + 29);
+//
+//	}
+//
+//	@DisplayName("메모삭제테스트")
+//	@Test
+//	void test4() {
+//		UserEntity testUser = getTestUser();
+//		Category category = getTestCategory();
+//		Memo savedMemo = memoRepository.save(Memo.create(testUser, category, "삭제될거임"));
+//
+//		assertThat(memoRepository.findAll().size()).isEqualTo(1);
+//		assertThatThrownBy(() -> memoService.deleteMemo(new CurrentUser(-1L), savedMemo.getId()))
+//			.isInstanceOf(Exception.class);
+//		memoService.deleteMemo(new CurrentUser(testUser.getId()), savedMemo.getId());
+//
+//		assertThat(memoRepository.findAll().size()).isEqualTo(0);
+//
+//	}
 
 }
