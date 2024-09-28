29ec9923795af419ef4fa2a09527ee80af698711
jiheon2234 <jiheon2234@gmail.com>
2024-09-28 19:56:49 +0900

    카테고리엔티티 카테고리아이콘링크 추가 카테고리 getSingleCategory 추가

--- src/main/java/com/shareduck/shareduck/domain/board/controller/CategoryController.java
+++ src/main/java/com/shareduck/shareduck/domain/board/controller/CategoryController.java
@@ -1,24 +1,16 @@
 package com.shareduck.shareduck.domain.board.controller;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.shareduck.shareduck.common.request.dto.CurrentUser;
 import com.shareduck.shareduck.domain.board.request.CategoryReq;
 import com.shareduck.shareduck.domain.board.response.CategoryRes;
 import com.shareduck.shareduck.domain.board.service.CategoryService;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -26,27 +18,33 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/categories")
 public class CategoryController {
 
-	private final CategoryService categoryService;
+    private final CategoryService categoryService;
 
-	@GetMapping("")
-	public ResponseEntity<List<CategoryRes>> listCategories(CurrentUser currentUser) {
-		List<CategoryRes> categories = categoryService.listUserCategories(currentUser);
+    @GetMapping("")
+    public ResponseEntity<List<CategoryRes>> listCategories(CurrentUser currentUser) {
+        List<CategoryRes> categories = categoryService.listUserCategories(currentUser);
 
-		return ResponseEntity.ok(categories); //TODO
-	}
+        return ResponseEntity.ok(categories); //TODO
+    }
 
-	@PostMapping("")
-	public ResponseEntity<CategoryRes> createCategory(CurrentUser currentUser,
-		@RequestBody @Valid CategoryReq categoryReq) {
-		CategoryRes categoryRes = categoryService.createCategory(currentUser.getUserId(), categoryReq);
-		return ResponseEntity.ok(categoryRes); //TODO
-	}
+    @PostMapping("")
+    public ResponseEntity<CategoryRes> createCategory(CurrentUser currentUser,
+                                                      @RequestBody @Valid CategoryReq categoryReq) {
+        CategoryRes categoryRes = categoryService.createCategory(currentUser.getUserId(), categoryReq);
+        return ResponseEntity.ok(categoryRes); //TODO
+    }
 
-	@PatchMapping("/{categoryId}")
-	public ResponseEntity<CategoryRes> updateCategory(CurrentUser currentUser, @PathVariable long categoryId,
-		@RequestBody CategoryReq categoryReq) {
-		CategoryRes categoryRes = categoryService.updateCategory(currentUser, categoryId, categoryReq);
-		return ResponseEntity.ok(categoryRes); //TODO
-	}
+    @PatchMapping("/{categoryId}")
+    public ResponseEntity<CategoryRes> updateCategory(CurrentUser currentUser, @PathVariable long categoryId,
+                                                      @RequestBody CategoryReq categoryReq) {
+        CategoryRes categoryRes = categoryService.updateCategory(currentUser, categoryId, categoryReq);
+        return ResponseEntity.ok(categoryRes); //TODO
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<CategoryRes> getSingleCategory(CurrentUser currentUser, @PathVariable long categoryId) {
+        CategoryRes categoryRes = categoryService.getSingleCategory(currentUser, categoryId);
+        return ResponseEntity.ok(categoryRes);
+    }
 
 }
--- src/main/java/com/shareduck/shareduck/domain/board/entity/Category.java
+++ src/main/java/com/shareduck/shareduck/domain/board/entity/Category.java
@@ -1,67 +1,64 @@
 package com.shareduck.shareduck.domain.board.entity;
 
-import static jakarta.persistence.FetchType.*;
-import static jakarta.persistence.GenerationType.*;
-import static lombok.AccessLevel.*;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import com.shareduck.shareduck.domain.user.persistence.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
-import com.shareduck.shareduck.domain.user.persistence.entity.UserEntity;
+import java.util.HashMap;
+import java.util.Map;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = PROTECTED)
 public class Category {
 
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
 
-	@Comment("카테고리 만든 유저 ID")
-	@ManyToOne(fetch = LAZY)
-	@JoinColumn(name = "user_id")
-	private UserEntity user;
+    @Comment("카테고리 만든 유저 ID")
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
 
-	@Comment("카테고리명")
-	private String name;
+    @Comment("카테고리명")
+    private String name;
 
-	@Column(name = "properties", columnDefinition = "json")
-	@JdbcTypeCode(SqlTypes.JSON)
-	private Map<String, Object> properties = new HashMap<>();
+    @Comment("카테고리아이콘")
+    private String categoryIcon;
 
-	public static Category create(UserEntity user, @NonNull String name, Map<String, Object> properties) {
-		Category category = new Category();
-		category.user = user;
-		category.setName(name);
-		category.properties = properties;
-		return category;
-	}
+    @Column(name = "properties", columnDefinition = "json")
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Map<String, Object> properties = new HashMap<>();
 
-	public void updateName(@NonNull String name) {
-		setName(name);
-	}
+    public static Category create(UserEntity user, @NonNull String name, String categoryIcon, Map<String, Object> properties) {
+        Category category = new Category();
+        category.user = user;
+        category.setName(name);
+        category.categoryIcon = categoryIcon;
+        category.properties = properties;
+        return category;
+    }
 
-	public void updateProperties(@NonNull Map<String, Object> properties) {
-		this.properties = properties;
-	}
+    public void updateName(@NonNull String name) {
+        setName(name);
+    }
 
-	private void setName(String name) {
-		this.name = name.trim();
-	}
+    public void updateProperties(@NonNull Map<String, Object> properties) {
+        this.properties = properties;
+    }
+
+    private void setName(String name) {
+        this.name = name.trim();
+    }
 
 }
